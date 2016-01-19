40b141ff2c9f2b11cd37a22efc518c78a08d0222
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2016-01-19 12:27:41 +0000

    Removed use of features and replaced String with Vec<u8> for large data element.

--- src/lib.rs
+++ src/lib.rs
@@ -1,152 +1,239 @@
+#![cfg(test)]
 #![feature(test, custom_derive, plugin)]
 #![plugin(serde_macros)]
 
-#![allow(dead_code)]
+extern crate rustc_serialize;
+extern crate serde;
 
-#[cfg(feature = "use_rustc_serialize")] extern crate rustc_serialize;
-#[cfg(feature = "use_serde")] extern crate serde;
-
-#[cfg(feature = "use_cbor")] extern crate cbor;
-#[cfg(feature = "use_bincode")] extern crate bincode;
+extern crate cbor;
+extern crate bincode;
 
 extern crate rand;
 extern crate test;
 
-#[cfg_attr(feature = "use_rustc_serialize", derive(RustcDecodable, RustcEncodable))]
-#[cfg_attr(feature = "use_serde", derive(Deserialize, Serialize))]
+#[derive(Deserialize, Serialize, RustcDecodable, RustcEncodable)]
 pub struct Person {
-  id:    u64,
-  name:  String,
-  email: String
+    id: u64,
+    name: String,
+    email: String,
 }
 
-#[cfg_attr(feature = "use_rustc_serialize", derive(RustcDecodable, RustcEncodable))]
-#[cfg_attr(feature = "use_serde", derive(Deserialize, Serialize))]
+#[derive(Deserialize, Serialize, RustcDecodable, RustcEncodable)]
 pub struct Document {
-  id:      u64,
-  name:    String,
-  authors: Vec<Person>,
-  content: String,
+    id: u64,
+    name: String,
+    authors: Vec<Person>,
+    content: Vec<u8>,
 }
 
-#[cfg(test)]
 fn make_sample_data(size: usize) -> Document {
-  use rand::{thread_rng, Rng};
+    use rand::{thread_rng, Rng};
 
-  let alice = Person {
-    id: 1,
-    name: "Alice".to_owned(),
-    email: "alice@example.com".to_owned()
-  };
+    let alice = Person {
+        id: 1,
+        name: "Alice".to_owned(),
+        email: "alice@example.com".to_owned(),
+    };
 
-  let bob = Person {
-    id: 2,
-    name: "Bob".to_owned(),
-    email: "bob@example.com".to_owned()
-  };
+    let bob = Person {
+        id: 2,
+        name: "Bob".to_owned(),
+        email: "bob@example.com".to_owned(),
+    };
 
-  Document {
-    id: 829472904,
-    name: "stuff.txt".to_owned(),
-    authors: vec![alice, bob],
-    content: thread_rng().gen_ascii_chars().take(size).collect(),
-  }
+    let mut rng = thread_rng();
+    let mut content =vec![0u8; size];
+    for i in content.iter_mut() {
+        *i = rng.gen();
+    }
+    Document {
+        id: 829472904,
+        name: "stuff.txt".to_owned(),
+        authors: vec![alice, bob],
+        content: content,
+    }
 }
 
-#[cfg(all(feature = "use_rustc_serialize", feature = "use_cbor"))]
-mod code {
-  use rustc_serialize::{Decodable, Encodable};
-  use cbor::{Decoder, Encoder};
+mod rustc_and_cbor {
+    use rustc_serialize::{Decodable, Encodable};
+    use cbor::{Decoder, Encoder};
 
-  pub fn encode<T: Encodable>(v: T) -> Vec<u8> {
-    let mut encoder = Encoder::from_memory();
-    encoder.encode(&[v]).unwrap();
-    encoder.as_bytes().to_vec()
-  }
+    pub fn encode<T: Encodable>(v: T) -> Vec<u8> {
+        let mut encoder = Encoder::from_memory();
+        encoder.encode(&[v]).unwrap();
+        encoder.as_bytes().to_vec()
+    }
 
-  pub fn decode<T: Decodable>(bytes: &[u8]) -> T {
-    let mut decoder = Decoder::from_bytes(bytes);
-    decoder.decode().next().unwrap().unwrap()
-  }
+    pub fn decode<T: Decodable>(bytes: &[u8]) -> T {
+        let mut decoder = Decoder::from_bytes(bytes);
+        decoder.decode().next().unwrap().unwrap()
+    }
 }
 
-#[cfg(all(feature = "use_serde", feature = "use_bincode"))]
-mod code {
-  use serde::{Deserialize, Serialize};
-  use bincode::SizeLimit;
-  use bincode::serde;
+mod serde_and_bincode {
+    use serde::{Deserialize, Serialize};
+    use bincode::SizeLimit;
+    use bincode::serde;
 
-  pub fn encode<T: Serialize>(v: T) -> Vec<u8> {
-    serde::serialize(&v, SizeLimit::Infinite).unwrap()
-  }
+    pub fn encode<T: Serialize>(v: T) -> Vec<u8> {
+        serde::serialize(&v, SizeLimit::Infinite).unwrap()
+    }
 
-  pub fn decode<T: Deserialize>(bytes: &[u8]) -> T {
-    serde::deserialize(bytes).unwrap()
-  }
+    pub fn decode<T: Deserialize>(bytes: &[u8]) -> T {
+        serde::deserialize(bytes).unwrap()
+    }
 }
 
-#[cfg(all(feature = "use_rustc_serialize", feature = "use_bincode"))]
-mod code {
-  use rustc_serialize::{Decodable, Encodable};
-  use bincode::SizeLimit;
-  use bincode::rustc_serialize;
+mod rustc_and_bincode {
+    use rustc_serialize::{Decodable, Encodable};
+    use bincode::SizeLimit;
+    use bincode::rustc_serialize;
 
-  pub fn encode<T: Encodable>(v: T) -> Vec<u8> {
-    rustc_serialize::encode(&v, SizeLimit::Infinite).unwrap()
-  }
+    pub fn encode<T: Encodable>(v: T) -> Vec<u8> {
+        rustc_serialize::encode(&v, SizeLimit::Infinite).unwrap()
+    }
 
-  pub fn decode<T: Decodable>(bytes: &[u8]) -> T {
-    rustc_serialize::decode(bytes).unwrap()
-  }
+    pub fn decode<T: Decodable>(bytes: &[u8]) -> T {
+        rustc_serialize::decode(bytes).unwrap()
+    }
 }
 
 #[cfg(test)]
 mod tests {
-  use super::{code, make_sample_data, Document};
-  use test::Bencher;
+    use super::{make_sample_data, Document};
+    use std::fmt;
+    use test::Bencher;
 
-  #[test]
-  fn sizes() {
-    println!("");
-    println!("Size after serialization:");
-    println!("    small: {} bytes", code::encode(&make_sample_data(0)).len());
-    println!("    big:   {} bytes", code::encode(&make_sample_data(1024 * 1024)).len());
-  }
+    enum Option {
+        RustcAndCbor,
+        SerdeAndBincode,
+        RustcAndBincode,
+    }
 
-  fn bench_serialize(b: &mut Bencher, size: usize) {
-    let doc = make_sample_data(size);
+    impl fmt::Debug for Option {
+        fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+            match *self {
+                Option::RustcAndCbor => write!(formatter, "Using CBOR format with rustc-serialize"),
+                Option::SerdeAndBincode => write!(formatter, "Using Bincode format with serde"),
+                Option::RustcAndBincode => write!(formatter, "Using Bincode format with rustc-serialize"),
+            }
+        }
+    }
 
-    b.iter(|| {
-      code::encode(&doc);
-    })
-  }
+    fn run_sizes(option: Option, small: &Document, big: &Document) {
+        println!("{:?}", option);
+        match option {
+            Option::RustcAndCbor => {
+                println!("    small: {} bytes", ::rustc_and_cbor::encode(small).len());
+                println!("    big:   {} bytes", ::rustc_and_cbor::encode(big).len());
+            }
+            Option::SerdeAndBincode => {
+                println!("    small: {} bytes", ::serde_and_bincode::encode(small).len());
+                println!("    big:   {} bytes", ::serde_and_bincode::encode(big).len());
+            }
+            Option::RustcAndBincode => {
+                println!("    small: {} bytes", ::rustc_and_bincode::encode(small).len());
+                println!("    big:   {} bytes", ::rustc_and_bincode::encode(big).len());
+            }
+        }
+    }
 
-  fn bench_deserialize(b: &mut Bencher, size: usize) {
-    let doc = make_sample_data(size);
-    let bytes = code::encode(&doc);
+    #[test]
+    fn sizes() {
+        println!("");
+        println!("Size after serialization:");
+        let small = make_sample_data(0);
+        let big = make_sample_data(1024 * 1024);
+        run_sizes(Option::RustcAndCbor, &small, &big);
+        run_sizes(Option::SerdeAndBincode, &small, &big);
+        run_sizes(Option::RustcAndBincode, &small, &big);
+    }
 
-    b.iter(|| {
-      code::decode::<Document>(&bytes);
-    })
-  }
+    fn bench_encode(bencher: &mut Bencher, option: Option, size: usize) {
+        let document = make_sample_data(size);
 
-  #[bench]
-  fn bench_serialize_small(b: &mut Bencher) {
-    bench_serialize(b, 0);
-  }
+        match option {
+            Option::RustcAndCbor => bencher.iter(|| ::rustc_and_cbor::encode(&document)),
+            Option::SerdeAndBincode => bencher.iter(|| ::serde_and_bincode::encode(&document)),
+            Option::RustcAndBincode => bencher.iter(|| ::rustc_and_bincode::encode(&document)),
+        }
+    }
 
-  #[bench]
-  fn bench_serialize_big(b: &mut Bencher) {
-    bench_serialize(b, 1024 * 1024);
-  }
+    fn bench_decode(bencher: &mut Bencher, option: Option, size: usize) {
+        let document = make_sample_data(size);
+        match option {
+            Option::RustcAndCbor => {
+                let bytes = ::rustc_and_cbor::encode(&document);
+                bencher.iter(|| ::rustc_and_cbor::decode::<Document>(&bytes))
+            }
+            Option::SerdeAndBincode => {
+                let bytes = ::serde_and_bincode::encode(&document);
+                bencher.iter(|| ::serde_and_bincode::decode::<Document>(&bytes))
+            }
+            Option::RustcAndBincode => {
+                let bytes = ::rustc_and_bincode::encode(&document);
+                bencher.iter(|| ::rustc_and_bincode::decode::<Document>(&bytes))
+            }
+        }
+    }
 
-  #[bench]
-  fn bench_deserialize_small(b: &mut Bencher) {
-    bench_deserialize(b, 0);
-  }
+    #[bench]
+    fn rustc_and_cbor_encode_small(bencher: &mut Bencher) {
+        bench_encode(bencher, Option::RustcAndCbor, 0);
+    }
 
-  #[bench]
-  fn bench_deserialize_big(b: &mut Bencher) {
-    bench_deserialize(b, 1024 * 1024);
-  }
+    #[bench]
+    fn serde_and_bincode_encode_small(bencher: &mut Bencher) {
+        bench_encode(bencher, Option::SerdeAndBincode, 0);
+    }
+
+    #[bench]
+    fn rustc_and_bincode_encode_small(bencher: &mut Bencher) {
+        bench_encode(bencher, Option::RustcAndBincode, 0);
+    }
+
+    #[bench]
+    fn rustc_and_cbor_encode_big(bencher: &mut Bencher) {
+        bench_encode(bencher, Option::RustcAndCbor, 1024 * 1024);
+    }
+
+    #[bench]
+    fn serde_and_bincode_encode_big(bencher: &mut Bencher) {
+        bench_encode(bencher, Option::SerdeAndBincode, 1024 * 1024);
+    }
+
+    #[bench]
+    fn rustc_and_bincode_encode_big(bencher: &mut Bencher) {
+        bench_encode(bencher, Option::RustcAndBincode, 1024 * 1024);
+    }
+
+    #[bench]
+    fn rustc_and_cbor_decode_small(bencher: &mut Bencher) {
+        bench_decode(bencher, Option::RustcAndCbor, 0);
+    }
+
+    #[bench]
+    fn serde_and_bincode_decode_small(bencher: &mut Bencher) {
+        bench_decode(bencher, Option::SerdeAndBincode, 0);
+    }
+
+    #[bench]
+    fn rustc_and_bincode_decode_small(bencher: &mut Bencher) {
+        bench_decode(bencher, Option::RustcAndBincode, 0);
+    }
+
+    #[bench]
+    fn rustc_and_cbor_decode_big(bencher: &mut Bencher) {
+        bench_decode(bencher, Option::RustcAndCbor, 1024 * 1024);
+    }
+
+    #[bench]
+    fn serde_and_bincode_decode_big(bencher: &mut Bencher) {
+        bench_decode(bencher, Option::SerdeAndBincode, 1024 * 1024);
+    }
+
+    #[bench]
+    fn rustc_and_bincode_decode_big(bencher: &mut Bencher) {
+        bench_decode(bencher, Option::RustcAndBincode, 1024 * 1024);
+    }
 }
